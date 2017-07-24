716fcf56b6c7984df471e4c75feddf4b2cea784b
victor-iyiola <javafolabi@gmail.com>
2017-07-25 00:41:53 +0100

    Finding document by id

--- test/finding_test.js
+++ test/finding_test.js
@@ -4,21 +4,33 @@ const assert = require('assert');
 // Find documents from the collection
 describe('Finding documents', () => {
   // Saves documents to be found
+  const firstname = 'Dave';
+  const lastname = 'McFarland';
+  const age = 43;
+  let dave;
   beforeEach((done) => {
-    const dave = new People({ firstname: 'Dave', lastname: 'McFarland', age: 43 });
+    dave = new People({ firstname, lastname, age });
     // Save this document
     dave.save().then(() => done() ).catch((err) => console.error(err) );
   });
 
   // Find people
-  it('finds credentials from collections', (done) => {
-    const firstname = 'Dave';
-    const lastname = 'McFarland';
+  it('finds document from collections', (done) => {
     People.findOne({ firstname: 'Dave' }).then((data) => {
       assert(data.firstname === firstname);
       done();
     }).catch((err) => {
-      console.log(`Could not find ${firstname}`);
+      console.error(`ERR: Could not find ${firstname} ${lastname}`);
+    });
+  });
+
+  // Finds by id
+  it('Finds document from collection by ID', (done) => {
+    People.findOne({ _id: dave._id }).then((data) => {
+      assert(data._id.toString() === dave._id.toString());
+      done();
+    }).catch((err) => {
+      console.err(`ERR: Could not find ${firstname} ${lastname}`)
     });
   });
 
