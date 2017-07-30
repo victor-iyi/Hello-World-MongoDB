5c106bc8fcd92198370d759e3f9a49a378cedb0e
victor-iyiola <javafolabi@gmail.com>
2017-07-30 17:50:32 +0100

    Code optimized.

--- test/database-test.js
+++ test/database-test.js
@@ -5,33 +5,33 @@ describe('Database connection', function() {
   const db = new Database('helloworld');
 
   // connect
-  it ('should connect to mongodb server.', function(done) {
-    db.connect(function(err) {
+  it ('should connect to mongodb server.', (done) => {
+    db.connect((err) => {
       expect(err).to.be.null;
       done();
     });
   });
 
   // create (insert)
-  it ('should insert document into database collection', function(done) {
+  it ('should insert document into database collection', (done) => {
     expect(true).to.be.true;
     done();
   });
 
   // read
-  it ('should read documents from a collection', function(done) {
+  it ('should read documents from a collection', (done) => {
     expect(true).to.be.true;
     done();
   });
 
   // update
-  it ('should a collection\'s document', function(done) {
+  it ('should a collection\'s document', (done) => {
     expect(true).to.be.ok;
     done();
   });
 
   // delete
-  it ('should delete document(s) from a collection', function(done) {
+  it ('should delete document(s) from a collection', (done) => {
     expect(true).to.be.ok;
     done();
   });
