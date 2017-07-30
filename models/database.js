642a638ae56120f5fe6e2aad0fa7ae49b737b0b2
victor-iyiola <javafolabi@gmail.com>
2017-07-30 17:47:12 +0100

    Database connection test passing!

--- models/database.js
+++ models/database.js
@@ -1,25 +1,36 @@
 const mongoose = require('mongoose');
 
+mongoose.Promise = global.Promise;
+
 function Database(name) {
   this.name = name;
 }
 
-
-// connect to the database
+// connect to mongodb
 Database.prototype.connect = function(cb) {
-  mongoose.connect(`mongodb://localhost/${this.name}`, { useMongoClient: true});
-  mongoose.connection.once('open', function() {
-    this.err = null;
-    cb(this.err);
-  }).on('err', function(err) {
-    this.err = err;
-    cb(this.err);
-  });
+  mongoose.connect(`mongodb://localhost/${this.name}`, { useMongoClient: true })
+    .then(() => cb(null))
+    .catch(err => cb(err));
 };
 
-// insert documents to collections.
+// create documents
 Database.prototype.insert = function() {
-  
+  //TODO: body...
+};
+
+// read documents
+Database.prototype.read = function() {
+  //TODO: body...
+};
+
+// update documents
+Database.prototype.update = function() {
+  //TODO: body...
+};
+
+// delete documents
+Database.prototype.delete = function() {
+  //TODO: body...
 };
 
 module.exports = Database;
